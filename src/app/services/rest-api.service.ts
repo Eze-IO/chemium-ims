dc1051f3bc7a962c3c718d36d8232d40e62883a7
Eze Ahuna <kryptowashere@gmail.com>
2020-04-01 22:14:21 -0500

    Few changes

--- src/app/services/rest-api.service.ts
+++ src/app/services/rest-api.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import { restapi } from '../models/restapi';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { response } from '../models/response';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+
   })
 }
 
@@ -16,10 +19,10 @@ export class RestAPIService {
 
   constructor(private http: HttpClient) { }
 
-  getEntity(entity: string):Observable<any> {
+  getEntity(entity: string):Observable<response> {
     const request = {
       'entity':entity
     };
-    return this.http.post<any>(restapi.getentity, request, httpOptions);
+    return this.http.post<response>(restapi.getentity, request, httpOptions);
   }
 }
