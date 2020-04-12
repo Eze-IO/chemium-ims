1c433d2af287eadc9e7d46c0de44059585bfe5f0
Eze Ahuna <kryptowashere@gmail.com>
2020-04-12 13:00:01 -0500

    Made changes to administrator service

---
+++ src/app/services/administrator.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { RestAPIService } from './rest-api.service';
+import { CurrentUserService } from './current-user.service';
+import { userinformation } from '../models/userinformation';
+import { user } from '../models/user';
+import { Type } from '../models/type';
+import { AuthenticationService } from './authentication.service';
+import { timer } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdministratorService {
+
+  private u: user = new user();
+  constructor(private ras: RestAPIService,
+              private cu: CurrentUserService) {
+                this.u = this.cu.GetInfo;
+              }
+
+  private _timer = null;
+  public get IsAdministrator(): boolean {
+    if(this._timer==null){
+      this._timer = timer(750, 30000).subscribe(x => {
+        this.u = this.cu.GetInfo;
+      })
+    }
+    return (this.u.type===Type.Administrator);
+  }
+
+  public async GetUsers() {
+    let list:userinformation[] = [];
+    if(this.IsAdministrator){
+      let result = await this.ras.ListUsers();
+      result.forEach(element => {
+          let u:userinformation = new userinformation();
+          u.name = element['Name'];
+          u.username = element['Username'];
+          u.email = element['Email'];
+          u.phone_number = element['PhoneNumber'];
+          u.lastmodified = element['LastModified'];
+          u.enabled = element['Enabled'];
+          u.created = element['Created'];
+          u.picture = element['Picture'];
+          u.status = element['Status'];
+          switch(element['Type']){
+            case 'administrator':
+            case 4:
+              u.type = 4;
+              break;
+            case 'editor':
+            case 2:
+              u.type = 2;
+              break;
+            default:
+              u.type = 0;
+              break;
+          }
+          list.push(u);
+      });
+    }
+    return list;
+  }
+
+  public IsUserConfirmed(u:user): boolean {
+    return !(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/.test(u.email));
+  }
+
+  public async CreateUser(user: user, password: string) {
+    if(this.IsAdministrator){
+      let fn = user.name.split(' ')[0];
+      let ln = user.name.split(' ')[1];
+      let result = await this.ras.UpdateRegister(fn, ln, user.email, password, user.phone_number, user.type);
+      return result;
+    }
+    return false;
+  }
+
+  public async DeleteUser(user: user) {
+    if(this.IsAdministrator){
+      let result = await this.ras.DeleteUser(user.email).then(x => { return x; });
+      return result;
+    }
+    return false;
+  }
+}
